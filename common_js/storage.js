6d40e95b57bf394d8a7b2a2bb5a173087efbbcf1
andrey <andreyantonov78@gmail.com>
2014-07-18 16:42:21 +0300

    change file storage (add file with _getRatings)

--- common_js/storage.js
+++ common_js/storage.js
@@ -2,16 +2,16 @@ var EEXCESS = EEXCESS || {};
 
 EEXCESS.storage = (function() {
     var _name = 'eexcess_db'; // name of the database
-    var _version = 42; // version number of the database
+    var _version = 43; // version number of the database
     var _db = {}; // the database (needs to be initalized - DO NOT ACCESS DIRECTLY! use _getDB instead)
-    
+
     // checks, if the provided callback is a function and executes it without parameters
     var _empty_callback = function(callback) {
         if (typeof callback === 'function') {
             callback();
         }
     };
-    
+
     // checks, if the provided callback is a function and executes it with supplied parameter
     var _optional_callback = function(callback, value) {
         if (typeof callback === 'function') {
@@ -23,7 +23,7 @@ EEXCESS.storage = (function() {
      * Wrapper for local storage access. If a value is supplied, it is stored at
      * with the supplied key. Otherwise the function returns the value stored at
      * the supplied key.
-     * 
+     *
      * @param {String} key The key for which to retrieve/store a value
      * @param {JSON} value (optional) The value to store. If no value is
      * supplied, the value at the supplied key is returned.
@@ -38,7 +38,7 @@ EEXCESS.storage = (function() {
 
     /**
      * Obtains a connection to the database
-     * 
+     *
      * @param {function} success success callback, receives the database as parameter
      * @param {function} error (optional) error callback
      */
@@ -51,9 +51,9 @@ EEXCESS.storage = (function() {
     };
 
     /**
-     * 
+     *
      * Puts an object into the desired object store (may overwrite an existing entry)
-     * 
+     *
      * @param {String} objectStore name of the object store, where the entry should be put in
      * @param {Object} value the value to store
      * @param {Function} error (optional) error callback
@@ -64,8 +64,12 @@ EEXCESS.storage = (function() {
             var tx = db.transaction(objectStore, 'readwrite');
             var store = tx.objectStore(objectStore);
             var req = store.put(value);
-            req.onsuccess = _empty_callback(success);
-            req.onerror = _empty_callback(error);
+            req.onsuccess = function() {
+                _empty_callback(success);
+            };
+            req.onerror = function() {
+                _empty_callback(error);
+            };
         }, _empty_callback(error));
     };
 
@@ -82,8 +86,12 @@ EEXCESS.storage = (function() {
             var tx = db.transaction(objectStore, 'readwrite');
             var store = tx.objectStore(objectStore);
             var req = store.add(value);
-            req.onsuccess = _empty_callback(success);
-            req.onerror = _empty_callback(error);
+            req.onsuccess = function() {
+                _empty_callback(success);
+            };
+            req.onerror = function() {
+                _empty_callback(error);
+            };
         }, _empty_callback(error));
     };
 
@@ -115,14 +123,16 @@ EEXCESS.storage = (function() {
                     _empty_callback(error);
                 }
             };
-            curreq.onerror = _empty_callback(error);
+            curreq.onerror = function() {
+                _empty_callback(error);
+            };
         }, _empty_callback(error));
     };
 
     /**
      * Stores the rating of a resource. If a rating for this resource is already
      * present within the same context, the rating is updated
-     * 
+     *
      * @param {Object} rating the rating, including the resource and context
      * @param {Function} success (optional) success callback without parameters
      * @param {Function} error (optional) error callback without parameters
@@ -155,20 +165,26 @@ EEXCESS.storage = (function() {
                     var tx2 = db.transaction('resource_relations', 'readwrite');
                     var store2 = tx2.objectStore('resource_relations');
                     var req = store2.put(rating);
-                    req.onsuccess = _empty_callback(success);
-                    req.onerror = _empty_callback(error);
+                    req.onsuccess = function() {
+                        _empty_callback(success);
+                    };
+                    req.onerror = function() {
+                        _empty_callback(error);
+                    };
                 }
             };
-            tx.onerror = _empty_callback(error);
+            tx.onerror = function() {
+                _empty_callback(error);
+            };
         }, _empty_callback(error));
     };
 
     /**
      * Obtains the rating for a resource from the database. At the moment, the
-     * rating's context is not considered (the same result item may be rated 
-     * different, when retrieved by different queries), but instead, the first 
+     * rating's context is not considered (the same result item may be rated
+     * different, when retrieved by different queries), but instead, the first
      * rating found is returned.
-     * 
+     *
      * @param {String} uri the uri of the resource for which to retrieve the rating
      * @param {Object} context the context for which to retrieve the rating
      * @param {Function} success success callback, receiving the rating score as parameter
@@ -198,13 +214,69 @@ EEXCESS.storage = (function() {
                     _optional_callback(error, 'no entry found');
                 }
             };
-            curreq.onerror = _optional_callback(error, 'db error');
+            curreq.onerror = function() {
+                _optional_callback(error, 'db error');
+            };
         }, _optional_callback(error, 'db error'));
     };
 
     /**
+     * Obtains ratings for a set of results from the database. At the moment, a
+     * rating's context is not considered (the same result item may be rated
+     * different, when retrieved by different queries), but instead, the first
+     * rating found is returned.
+     *
+     * @param {Array} items Array of result objects for which to retrieve ratings
+     * @param {Function} success success callback, receiving the updated results as parameter
+     * @param {Function} error (optional) error callback
+     * @returns {undefined}
+     */
+    var _getRatings = function(items, success, error) {
+        _getDB(function(db) {
+            var tx = db.transaction('resource_relations');
+            var store = tx.objectStore('resource_relations');
+            var idx = store.index('resource');
+
+            var i = 0;
+            handleNext(); // initial item (others get handled by callback on success)
+            // handle a single item
+            function handleNext() {
+                if (i < items.length) {
+                    var curreq = idx.openCursor(items[i].uri);
+                    curreq.onsuccess = function() {
+                        var cursor = curreq.result;
+                        if (cursor) { // TODO: check context?
+                            var req = store.get(cursor.primaryKey);
+                            req.onsuccess = function() {
+                                if (typeof req.result !== 'undefined') {
+                                    if (req.result.type === 'rating') { // TODO: check context?
+                                        items[i].rating = req.result.annotation.hasBody['http://purl.org/stuff/rev#rating'];
+                                        i++;
+                                        handleNext();
+                                    } else {
+                                        cursor.continue();
+                                    }
+                                }
+                            };
+                        } else {
+                            i++;
+                            handleNext();
+                        }
+                    };
+                }
+            }
+            tx.oncomplete = function() {
+                success(items);
+            };
+            tx.onerror = function() {
+                _empty_callback(error);
+            };
+        });
+    };
+
+    /**
      * Enriches a visit with its referrer (if any) and stores it in the database
-     * 
+     *
      * @param {Object} visitItem the visit to store
      * @param {Integere} referringVisitId identifier of the referring visit
      */
@@ -227,9 +299,9 @@ EEXCESS.storage = (function() {
     };
 
     /**
-     * Stores recommendations in the database, along with the context in which 
+     * Stores recommendations in the database, along with the context in which
      * they were retrived and a timestamp.
-     * 
+     *
      * @param {Array} recommendations the recommendations to store
      * @param {Object} context the context in which the recommendations were retrieved
      * @param {long} timestamp the timestamp
@@ -270,7 +342,7 @@ EEXCESS.storage = (function() {
 
     /**
      * Initializes or updates the database. If the rm_previous parameter is set to true or left empty, the previous entries are removed.
-     * 
+     *
      * @param {function} success (optional) success callback (receives the database as parameter)
      * @param {function} error (optional) error callback (receives the error object as parameter)
      * @param {type} rm_previous (optional) flag, indicating if the previous entries should be removed (by default they are)
@@ -323,6 +395,17 @@ EEXCESS.storage = (function() {
             os = EEXCESS.DB.createObjectStore('queries', {keyPath: 'id', autoIncrement: true});
             os.createIndex('query', 'query');
             os.createIndex('timestamp', 'timestamp');
+
+
+            // remove existing object store 'queries_full' if present
+            if (EEXCESS.DB.objectStoreNames.contains('queries_full') && clear) {
+                EEXCESS.DB.deleteObjectStore('queries_full');
+            }
+            // create object store 'queries_full'
+            os = EEXCESS.DB.createObjectStore('queries_full', {keyPath: 'id', autoIncrement: true});
+            os.createIndex('query', 'query');
+            os.createIndex('timestamp', 'timestamp');
+
 
             // remove existing object store 'history' if present
             if (EEXCESS.DB.objectStoreNames.contains('history') && clear) {
@@ -427,6 +510,7 @@ EEXCESS.storage = (function() {
         storeVisit: _storeVisit,
         storeRecommendations: _storeRecommendations,
         getRating: _getRating,
+        getRatings: _getRatings,
         setRating: _setRating,
         closedRecommendation: _closedRecommendation,
         loadQueryCrumbsData: _loadQueryCrumbsData
